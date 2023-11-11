71fed463ff2dbef6dc1d1ba1b24ff437542d1bf7
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-11 14:41:36 +0100

    Update chess_board.py

--- chess_board.py
+++ chess_board.py
@@ -20,7 +20,7 @@
 from prettytable import PrettyTable
 
 chessboard_reference = []
-chessboard_actual = ["\u2656","\u2658","\u2657","\u2655","\u2654","\u2657","\u2658","\u2656",
+chessboard_start = ["\u2656","\u2658","\u2657","\u2655","\u2654","\u2657","\u2658","\u2656",
                      "\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659",
                      "","","","","","","","",
                      "","","","","","","","",
@@ -29,14 +29,44 @@ chessboard_actual = ["\u2656","\u2658","\u2657","\u2655","\u2654","\u2657","\u26
                      "\u265F","\u265F","\u265F","\u265F","\u265F","\u265F","\u265F","\u265F",
                      "\u265C","\u265E","\u265D","\u265B","\u265A","\u265D","\u265E","\u265C"]
 
-board = PrettyTable()
-board.field_names = ["","A","B","C","D","E","F","G","H"]
-board.add_row(["1"] + chessboard_actual[0:8:])
-board.add_row(["2"] + chessboard_actual[8:16:])
-board.add_row(["3"] + chessboard_actual[16:24:])
-board.add_row(["4"] + chessboard_actual[24:32:])
-board.add_row(["5"] + chessboard_actual[32:40:])
-board.add_row(["6"] + chessboard_actual[40:48:])
-board.add_row(["7"] + chessboard_actual[48:56:])
-board.add_row(["8"] + chessboard_actual[56:65:])
-print(board)
+chessboard_actual = chessboard_start.copy()
+
+letter = "B"
+number = 1
+
+def convert_coordinate_index(let, num):
+    if let == "A":
+        num -= 1
+    if let == "B":
+        num += 7
+    if let == "C":
+        num += 14
+    if let == "D":
+        num += 21
+    if let == "E":
+        num += 28
+    if let == "F":
+        num += 35
+    if let == "G":
+        num += 42
+    if let == "H":
+        num += 49
+    return num
+
+
+
+
+def display():
+    board = PrettyTable()
+    board.field_names = ["","A","B","C","D","E","F","G","H"]
+    board.add_row(["1"] + chessboard_actual[0:8:])
+    board.add_row(["2"] + chessboard_actual[8:16:])
+    board.add_row(["3"] + chessboard_actual[16:24:])
+    board.add_row(["4"] + chessboard_actual[24:32:])
+    board.add_row(["5"] + chessboard_actual[32:40:])
+    board.add_row(["6"] + chessboard_actual[40:48:])
+    board.add_row(["7"] + chessboard_actual[48:56:])
+    board.add_row(["8"] + chessboard_actual[56:65:])
+    print(board)
+
+display()
